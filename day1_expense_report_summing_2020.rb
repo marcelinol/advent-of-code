48316762fdccce996cdb213ca9149b86cee97b8d
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2021-01-21 16:21:34 -0500

    Minimal solution of Day 1 puzzle

---
+++ day1_expense_report_summing_2020.rb
@@ -0,0 +1,38 @@
+# the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+#
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+#
+# For example, suppose your expense report contained the following:
+#
+# 1721
+# 979
+# 366
+# 299
+# 675
+# 1456
+#
+# In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+# Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+class ExpenseReportPuzzle
+  def self.run(input)
+    input.each_with_index do |pivot, index|
+      input.slice(index..-1).each do |number|
+        return pivot * number if pivot + number === 2020
+      end
+    end
+
+    raise StandardError, "No numbers found in the input that sum 2020"
+  end
+end
+
+require 'benchmark'
+# This report shows the user CPU time, system CPU time, the sum of the user and system CPU times, and the elapsed real time. The unit of time is seconds.
+official_input = File.open("day1_input.txt").map(&:to_i);false
+puts Benchmark.measure { ExpenseReportPuzzle.run(official_input) }
+
+puts ExpenseReportPuzzle.run(official_input)
+
+
+## ideas
+# remove numbers higher than 2019?
---
+++ day1_expense_report_summing_2020_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './day1_expense_report_summing_2020.rb'
+
+describe 'find values that sum 2020' do
+  context 'with two values in the list' do
+    it 'throws an error if no values match 2020' do
+      input = [1, 1]
+      expect { ExpenseReportPuzzle.run(input) }.to raise_error(StandardError, 'No numbers found in the input that sum 2020')
+    end
+
+    it 'works with two numbers' do
+      input = [1_000, 1_020]
+      expect(ExpenseReportPuzzle.run(input)).to eq(1_020_000)
+    end
+
+    it 'works with more than two numbers' do
+      input = [1_000, 3, 1_020]
+      expect(ExpenseReportPuzzle.run(input)).to eq(1_020_000)
+    end
+  end
+end
