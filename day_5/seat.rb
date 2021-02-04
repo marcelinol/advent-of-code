b5fae668c5354a5adf3c211d44ec9e6d8f7813ed
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2021-02-04 14:44:01 -0500

    Solve day 5 part 2

--- day_5/seat.rb
+++ day_5/seat.rb
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2020/day/5
+
 class Seat
   def initialize(seat_code)
     @binary_seat_code = seat_code.gsub(/[FBLR]/, 'F' => 0, 'B' => '1', 'L' => 0, 'R' => '1')
@@ -12,14 +14,53 @@ class Seat
   end
 
   def seat_id
+    # Not happy with this
+    self.class.calculate_seat_id(row: row, column: column)
+  end
+
+  def self.calculate_seat_id(row:, column:)
     row * 8 + column
   end
 end
 
-official_input = File.open("day_5/input.txt")
-highest_seat_id = 0
-official_input.each do |seat_code|
-  seat = Seat.new(seat_code)
-  highest_seat_id = seat.seat_id if seat.seat_id > highest_seat_id
+class EmptySeatFinder
+  def initialize(seats)
+    @seats = seats
+  end
+
+  # Not very happy with this
+  def find_seat_id
+    grouped_seats_by_row = group_seats_by_row(@seats)
+    grouped_seats_by_row.each do |row, seats|
+      next if seats.size == 8
+
+      seat_columns = seats.map(&:column)
+      empty_seat_columns = (0..7).to_a - seat_columns
+      empty_seat_column = (empty_seat_columns - [0,7])[0] # this returns an array. I want its first (and only) element
+
+      return Seat.calculate_seat_id(row: row, column: empty_seat_column)
+    end
+  end
+
+  private
+  def group_seats_by_row(seats)
+    result = {}
+    seats.each do |seat|
+      result[seat.row] ||= []
+      result[seat.row] << seat
+    end
+
+    result
+  end
 end
-puts highest_seat_id
+
+official_input = File.open("day_5/input.txt")
+# highest_seat_id = 0
+# official_input.each do |seat_code|
+#   seat = Seat.new(seat_code)
+#   highest_seat_id = seat.seat_id if seat.seat_id > highest_seat_id
+# end
+# puts highest_seat_id
+#
+# empty_seat_finder = EmptySeatFinder.new(official_input.map { |seat_code| Seat.new(seat_code) })
+# puts empty_seat_finder.find_seat_id
--- day_5/seat_spec.rb
+++ day_5/seat_spec.rb
@@ -2,24 +2,43 @@ require 'rspec'
 require_relative 'seat'
 
 describe 'Day 5', :aggregate_failures do
-  it 'gets row from seat code' do
-    expect(Seat.new('FBFBBFFRLR').row).to eq(44)
-    expect(Seat.new('BFFFBBFRRR').row).to eq(70)
-    expect(Seat.new('FFFBBBFRRR').row).to eq(14)
-    expect(Seat.new('BBFFBBFRLL').row).to eq(102)
+  describe 'Seat' do
+    it 'gets row from seat code' do
+      expect(Seat.new('FBFBBFFRLR').row).to eq(44)
+      expect(Seat.new('BFFFBBFRRR').row).to eq(70)
+      expect(Seat.new('FFFBBBFRRR').row).to eq(14)
+      expect(Seat.new('BBFFBBFRLL').row).to eq(102)
+    end
+
+    it 'gets column from seat code' do
+      expect(Seat.new('FBFBBFFRLR').column).to eq(5)
+      expect(Seat.new('BFFFBBFRRR').column).to eq(7)
+      expect(Seat.new('FFFBBBFRRR').column).to eq(7)
+      expect(Seat.new('BBFFBBFRLL').column).to eq(4)
+    end
+
+    it 'gets seat id from seat code' do
+      expect(Seat.new('FBFBBFFRLR').seat_id).to eq(357)
+      expect(Seat.new('BFFFBBFRRR').seat_id).to eq(567)
+      expect(Seat.new('FFFBBBFRRR').seat_id).to eq(119)
+      expect(Seat.new('BBFFBBFRLL').seat_id).to eq(820)
+    end
   end
 
-  it 'gets column from seat code' do
-    expect(Seat.new('FBFBBFFRLR').column).to eq(5)
-    expect(Seat.new('BFFFBBFRRR').column).to eq(7)
-    expect(Seat.new('FFFBBBFRRR').column).to eq(7)
-    expect(Seat.new('BBFFBBFRLL').column).to eq(4)
-  end
+  describe 'EmptySeatFinder' do
+    it 'finds empty seat in row 44, column 2' do
+      seats = [
+        Seat.new('FBFBBFFLLL'),
+        Seat.new('FBFBBFFLLR'),
+        # Empty seat: FBFBBFFLLR
+        Seat.new('FBFBBFFLRR'),
+        Seat.new('FBFBBFFRLL'),
+        Seat.new('FBFBBFFRLR'),
+        Seat.new('FBFBBFFRRL'),
+        Seat.new('FBFBBFFRRR'),
+      ]
 
-  it 'gets seat id from seat code' do
-    expect(Seat.new('FBFBBFFRLR').seat_id).to eq(357)
-    expect(Seat.new('BFFFBBFRRR').seat_id).to eq(567)
-    expect(Seat.new('FFFBBBFRRR').seat_id).to eq(119)
-    expect(Seat.new('BBFFBBFRLL').seat_id).to eq(820)
+      expect(EmptySeatFinder.new(seats).find_seat_id).to eq(354)
+    end
   end
 end
